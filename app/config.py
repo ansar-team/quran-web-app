5f7aab529467407d18d13312343c0a66460f2977
Ansarello <t83747064@gmail.com>
2025-10-09 16:16:35 +0300

    feat: setup PostgreSQL connection and SQLAlchemy model for User

---
+++ app/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    # Database
+    database_url: str = "postgresql://username:password@localhost:5432/spaced_repetition_db"
+
+    # Environment
+    debug: bool = True
+
+    class Config:
+        env_file = "../.env"
+        case_sensitive = False
+
+
+settings = Settings()
